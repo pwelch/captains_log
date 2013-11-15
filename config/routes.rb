10b69e5629f7450f83c5be8ff38d77566257c297
Paul Welch <paul@pwelch.net>
2013-11-14 20:25:43 -0500

    Finish Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 CaptainsLog::Application.routes.draw do
   get "static_pages/home"
+  get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -40,7 +41,7 @@ CaptainsLog::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should hvae the content 'CLog'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Home')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("CLog | Home")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About'" do
+      visit "/static_pages/about"
+      expect(page).to have_content('About')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("CLog | About")
+    end
+  end
+end
